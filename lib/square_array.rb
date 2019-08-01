5331c0c2f611a0134faa95568682055372f2731e
John Guest <jcguest90@gmail.com>
2019-08-01 05:58:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
-  square = []
-  count = 0 
-  while count < array.size do 
-    squared (array[count] ** 2)
-    count += 1 
-    
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
   end
-  square
+  squared
 end
+square_array(123,1284)
